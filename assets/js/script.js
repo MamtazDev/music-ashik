cb4d0bbb59fbb2e15d31dfe06383c4bbb54fe607
unknown <getintouch.ashiq@gmail.com>
2023-09-01 18:39:25 -0700

    music landing

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * @Script js for (Template/Project Name)
+ * @project     - Project Name
+ * @author      - Ashiqur Rahman
+ * @created_by  -
+ * @created_at  -
+ * @modified_by -
+ **/
+
+$(window).on("load", function () {
+  // code should be execute here
+});
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+  /* code execute from here */
+});
